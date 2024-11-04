ef9cf82c3d771f20cc6195caa2cec0285200e4b3
vxndtt <alenakad16@mail.ru>
2024-11-04 17:43:42 +0500

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,6 +1,5 @@
 import multiprocessing
 from datetime import datetime
-from multiprocessing import Pool
 
 
 def read_info(name):
@@ -16,22 +15,22 @@ def read_info(name):
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-#time_start = datetime.now()
+time_start = datetime.now()
 
-#for file in filenames:
-    #read_info(file)
+for file in filenames:
+    read_info(file)
 
-#time_stop = datetime.now()
-# = time_stop - time_start
-#print(time_res)
-
-time_start2 = datetime.now()
+time_stop = datetime.now()
+time_res = time_stop - time_start
+print(time_res)
 
 if __name__ == '__main__':
+
+    time_start2 = datetime.now()
 
     with multiprocessing.Pool() as pool:
         pool.map(read_info, filenames)
 
-time_stop2 = datetime.now()
-time_res2 = time_stop2 - time_start2
-print(time_res2)
+    time_stop2 = datetime.now()
+    time_res2 = time_stop2 - time_start2
+    print(time_res2)
